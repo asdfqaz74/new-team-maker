63ebb0fb0e9f09902dad92c1c1227c502b289b9b
KangJS <asdfqaz1996@eco.co.kr>
2025-12-23 17:03:07 +0900

    style: TeamMaker 컴포넌트의 헤더 크기 및 레이아웃 스타일 개선

--- src/app/team-maker/page.tsx
+++ src/app/team-maker/page.tsx
@@ -3,10 +3,10 @@ import { ChooseBox } from "@/components/team-maker";
 const TeamMaker = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center">
-      <h2 className="text-6xl font-bold mb-20 text-center">
+      <h2 className="text-3xl md:text-6xl font-bold mb-20 text-center">
         팀 선정 방식을 선택해주세요
       </h2>
-      <div className="flex gap-10 justify-center">
+      <div className="flex flex-col md:flex-row gap-10 justify-center px-2">
         <ChooseBox type="select" />
         <ChooseBox type="ai" />
       </div>
